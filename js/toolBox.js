e0270bd786c57ef435123057ef48c7f4122512af
HanneSips <75627390+HanneSips@users.noreply.github.com>
2022-09-09 00:12:48 +0200

    Update toolBox.js

--- js/toolBox.js
+++ js/toolBox.js
@@ -1,7 +1,7 @@
 import Polyhedron from "./polyhedron.js";
 import LightSource from "./lightSource.js";
 import Projection from "./projection.js";
-import Vertex from "./Vertex.js";
+import Vertex from "./vertex.js";
 import ProjectionPlane from "./projectionPlane.js";
 import {width, height, MAX_X, MAX_Y, MAX_Z, tool_wrapper} from "./globals.js"
 import { setupUI } from "./userInteraction.js";
